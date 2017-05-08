0191bff086bc4c81e3a8b09fb3c63540731a7384
dickymicky <dickymicky@naver.com>
2017-05-08 22:50:16 +0900

    JS Insert

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+(function($){
+
+    $('a[href="#"]').click(function(event){
+        event.preventDefault();
+    });
+
+    var $tab = $('header ul li a'),
+        $nav = $('header p a');
+
+    $tab.on('click', function(){
+        $(this).addClass('act').parent().siblings().children('a').removeClass('act');
+    });
+
+    // SideMenu
+    $nav.on('click', function(){
+        $(this).parents().next().stop(false, true).animate({
+            width: 'toggle'
+        }, 400);
+    });
+
+})(jQuery);
